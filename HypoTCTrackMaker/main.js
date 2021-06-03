97dc9a60c0ac5935f1be1ad008368c93162c07b1
wilburcoding <66228004+wilburcoding@users.noreply.github.com>
2021-06-03 09:23:59 -0400

    Update main.js

--- HypoTCTrackMaker/main.js
+++ HypoTCTrackMaker/main.js
@@ -4,7 +4,7 @@ var numberofdots=0;
 const WIDTH = 960;
 const HEIGHT = 540;
 const MAP_WIDTH = 5000;
-const MAP_HEIGHT = MAP_WIDTH/2+50;
+const MAP_HEIGHT = 1580;
 const COLORS = ['#5ebaff','#00faf4','#ffffcc','#ffe775','#ffc140','#ff8f20','#ff6060','#FF0097','#FF00D4','#FB00FF','#D500FF','#AA00FF','#9B9B8B'];
 var numberofstorms = 0;
 var loadedMapImg,
